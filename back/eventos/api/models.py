f62b963c77802ba143b15ddb6958f7e1c91c6d30
jssosa10 <js.sosa10@uniandes.edu.co>
2020-08-12 00:04:34 -0500

    order desc by creation date

--- back/eventos/api/models.py
+++ back/eventos/api/models.py
@@ -35,7 +35,7 @@ class Event(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-         ordering = ['created_at']
+         ordering = ['-created_at']
 
     def __str__(self):
         return self.name
