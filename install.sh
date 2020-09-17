9acba272ffe1d3bba33af0d4a73d4ca4a399fd17
jssosa10 <js.sosa10@uniandes.edu.co>
2020-09-17 17:39:04 -0500

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo apt update
 sudo apt install docker.io
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
